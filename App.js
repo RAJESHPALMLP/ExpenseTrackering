fbc7ac5d4e5c3e0b272fa1d7fca9a5669c0964a9
RAJESHPALMLP <79746918+RAJESHPALMLP@users.noreply.github.com>
2022-11-12 10:56:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,62 @@
+
+import React ,{useState}from 'react';
+
+import NewExpense from './components/NewExpenses/NewExpense';
+import Expenses  from './components/Expenses/Expenses';
+let DUMMY_EXPENSE=[
+
+    {
+        id :'e1',
+        title: 'School Free',
+        amount : 250,
+        date :new Date(2021,5,12)
+    },
+    {
+        id :'e2',
+        title: 'Books',
+        amount : 230,
+        date :new Date(2021,2,22)
+    },
+    {
+        id :'e3',
+        title: 'House Rent',
+        amount : 700,
+        date :new Date(2021,4,12)
+    },
+    {
+        id :'e4',
+        title: 'Food',
+        amount : 500,
+        date :new Date(2021,5,12)
+    }
+];
+
+const App=()=>
+{
+
+    const [expenses,setExpenses]=useState(DUMMY_EXPENSE);
+    
+
+    const addExpenseHandler=(expense)=>{
+        const updateExpense=[expense,...expenses];
+        setExpenses(updateExpense)
+    };
+
+       
+    
+    return (
+         <div>
+            <NewExpense onAddExpense={addExpenseHandler}/>
+            <Expenses item={expenses}/>
+
+            
+            
+         </div>
+
+    );
+}
+export default App;
+
+
+
+
